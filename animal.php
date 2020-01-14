cdcf55d24d83edbac5cfa02f0b672f11287be833
ayumi-sato-046 <55998071+ayumi-sato-046@users.noreply.github.com>
2020-01-14 18:37:53 +0900

    animal.php 完成

--- animal.php
+++ animal.php
@@ -2,18 +2,23 @@
 
 class Animal {
     //名前を格納する変数と年齢を格納する変数
-    public $name = 'Mr Tanaka';
-    public $age = 25;
-    
+    public $name = '';
+    public $age = '';
+     
+     function __construct($name, $age, $suffix = '') {
+        $this->name = $name;
+        $this->age = $age;
+        $this->suffix = $suffix;
+     }
     //say という名前の関数
-    function say() {
+    function say($animal) {
         print $this->name . 'です。' . $this->age . '歳です。' . PHP_EOL;
+
     }
 }
-$animal = new Animal();
-$animal->say();
 
-// 以下の2行は動作確認のための記述です。確認できたらコメントアウトしておきましょう。
+//以下の2行は動作確認のための記述です。確認できたらコメントアウトしておきましょう
 //$animal = new Animal("田中 太郎", 25);
-//$animal->say();
+//$animal->say($animal);
+
 ?>
