6569aeab1e0517e0af23cb8df3ea8c1ff1ee0c13
ayumi-sato-046 <55998071+ayumi-sato-046@users.noreply.github.com>
2020-01-14 22:27:20 +0900

    human.php 完成！

--- human.php
+++ human.php
@@ -7,15 +7,17 @@ require_once 'thinkable.php';
 require_once 'animal.php';
 class Human extends Animal {
 
+//トレイトをuseする
+use Thinkable;
 //趣味の情報を格納する変数を定義
     public $hobby = "";
-   
+
 //コンストラクタに名前・年齢・趣味の情報が入る引数を設定し、名前・年齢・趣味の各変数を引数の情報で初期化する 
     function __construct($name, $age, $hobby) {
         $this->name = $name;
         $this->age = $age;
         $this->hobby = $hobby;
-        
     }
-} 
+
+}
 ?>
