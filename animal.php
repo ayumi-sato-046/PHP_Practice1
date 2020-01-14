20acc27f3fd75dd811eba8d70a87c390134973dd
ayumi-sato-046 <55998071+ayumi-sato-046@users.noreply.github.com>
2020-01-14 22:25:23 +0900

    animal.php 完成！

--- animal.php
+++ animal.php
@@ -5,13 +5,13 @@ class Animal {
     public $name = '';
     public $age = '';
      
-     function __construct($name, $age, $suffix = '') {
+     function __construct($name, $age) {
         $this->name = $name;
         $this->age = $age;
-        $this->suffix = $suffix;
+        
      }
     //say という名前の関数
-    function say($animal) {
+    function say() {
         print $this->name . 'です。' . $this->age . '歳です。' . PHP_EOL;
 
     }
@@ -19,6 +19,6 @@ class Animal {
 
 //以下の2行は動作確認のための記述です。確認できたらコメントアウトしておきましょう
 //$animal = new Animal("田中 太郎", 25);
-//$animal->say($animal);
+//$animal->say();
 
 ?>
